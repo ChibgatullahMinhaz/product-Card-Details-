cbc1504002132ec72b73bb0d1a5be94d097bd48b
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-03-02 23:26:44 +0600

    add to card

--- script.js
+++ script.js
@@ -7,30 +7,40 @@ for (let i = 0; i < ringBtn.length; i++) {
         }
         event.target.classList.add('border-purple-700');
         const color = event.target.id.replace('-color', '');
-        console.log(color);
-        let image = document.getElementById('product-image');
-        image.src = './images/' + color + '.png';
+        return image = document.getElementById('product-image').src = './images/' + color + '.png';
+        
     })
 }
 // select size box 
-function selectSizeBox(size){
+function selectSizeBox(size) {
     const sizes = ['M', 'S', 'L', 'XL'];
     for (const siz of sizes) {
         const boxBtn = document.getElementById('size-' + siz)
         if (size === siz) {
             boxBtn.classList.add('border-purple-600')
-        }else{
+        } else {
             boxBtn.classList.remove('border-purple-600')
 
         }
     }
 }
 // handle quantity
-
 document.querySelectorAll('.quantity-button').forEach(btn => {
-    btn.addEventListener('click', (e)=>{
-        const Quentity =parseFloat(document.getElementById('quantity').innerText);
-        const amount =  e.target.innerText === "+" ? 1 : -1;
+    btn.addEventListener('click', (e) => {
+        const Quentity = parseFloat(document.getElementById('quantity').innerText);
+        const amount = e.target.innerText === "+" ? 1 : -1;
         document.getElementById('quantity').innerText = Math.max(0, Quentity + amount)
     })
 });
+
+// add to cart
+let chekoutCount = 0;
+document.getElementById('add-to-cart').addEventListener('click', () => {
+    const Quentity = parseFloat(document.getElementById('quantity').innerText);
+    if (Quentity === 0) {
+       return alert("please selec a Quentity...")
+    }
+    chekoutCount  = chekoutCount + Quentity;
+    document.getElementById('checkout-container').classList.remove('hidden');
+    document.getElementById('cart-count').innerText= chekoutCount;
+})
